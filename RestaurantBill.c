48080ad16083ef92b814d9bbd2e81ca7938591b1
Jeremy Embar <jembar@cpp.edu>
2022-02-28 14:26:42 -0800

    Initial commit

---
+++ RestaurantBill.c
@@ -0,0 +1,18 @@
+/** RestaurantBill.c
+ * 
+ * TODO:
+ * Add command line arguments to main for the tax and tip
+ *  - the number of arguments should be 2, the tax and tip in that order, if it's not 2 program should end
+ *  - they are given as percentages, with the percent symbol being optional
+ *  - convert the arguments to float, if it isn't possible program should end
+ * 
+ * Add random meal cost, choose from values $9.95, $4.55, $13.25, $22.35
+ * Calculate cost with tax and tip
+ * Format and display meal cost, tax amount, tip amount, total bill
+ * 
+ * Output format example:
+ * Meal cost: $9.95
+ * Tax amount: 10%
+ * Tip amount: 15%
+ * Total: $12.59
+ */
