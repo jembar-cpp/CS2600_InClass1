55d247c26bf4eb6f17bef135a98a48ac3a570a44
Jeremy Embar <jembar@cpp.edu>
2022-02-28 14:55:36 -0800

    added random meal cost, fixed formatting issue

--- RestaurantBill.c
+++ RestaurantBill.c
@@ -13,10 +13,11 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(int argc, char *argv[]) {
     if (argc != 3) { // Make sure number of command line arguments is correct
-        printf("Incorrect number of command line arguments specified.");
+        printf("Incorrect number of command line arguments specified.\n");
         printf("Enter the arguments as percentages in this order: tax, tip");
         return -1;
     }
@@ -25,5 +26,8 @@ int main(int argc, char *argv[]) {
     float tax = atof(argv[1]) / 100;
     float tip = atof(argv[2]) / 100;
 
-    printf("%f,%f",tax,tip);
+    // Generate random meal cost
+    srand(time(NULL)); // use time for a random seed
+    float mealCosts[4] = {9.95, 4.55, 13.25, 22.35};
+    float mealCost = mealCosts[rand() % 4];
 }
