796673eaea6fef19eeb74de2fc5122f5205e833f
Jeremy Embar <jembar@cpp.edu>
2022-02-28 21:44:35 -0800

    initial commit for NumberGuessingGame

---
+++ NumberGuessingGame.c
@@ -0,0 +1,17 @@
+/** NumberGuessingGame.c
+ * 
+ * Program which allows the user to guess a number between 1 and 10 (or a different user-specified maximum)
+ * 
+ * TODO:
+ * Implement function to printMenu() to print menu, prompt user input, and act based on input:
+ *  - Press 1 to play a game
+ *  - Press 2 to change the max number
+ *  - Press 3 to quit
+ * 
+ * Implement function guessNumber(int answer) that loops, asking user to guess the number:
+ *  - prints higher or lower based on number
+ *  - if user wins, return to menu
+ *  - if q is pressed, game ends (counts as loss) and returns to menu
+ * 
+ * On program end, display all games and how many guesses it took (or if they quit)
+ */
