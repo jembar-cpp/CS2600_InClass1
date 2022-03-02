5399bbe513eec8b7d7b12f747b1174fd53a7c958
Jeremy Embar <jembar@cpp.edu>
2022-03-02 15:54:58 -0800

    changed to pre-tax amount

--- RestaurantBill.c
+++ RestaurantBill.c
@@ -1,7 +1,7 @@
 /** RestaurantBill.c
  * 
  * Calculates the total cost of a randomly-selected meal based on tax and tip.
- * Tip is on the post-tax amount of the meal.
+ * Tip is on the pre-tax amount of the meal.
  * Tax and tip are specified as command-line arguments, with or without a percent symbol.
  * Tax and tip are assumed to be integer amounts (10% or 15%, not 17.5%).
  */
@@ -22,16 +22,19 @@ int main(int argc, char *argv[]) {
 
     // Generate random meal cost
     srand(time(NULL)); // use time for a random seed
+    int randIndex = rand() % 4;
+    const char meals[4][10] = {"Salad", "Soup", "Sandwich", "Pizza"};
     float mealCosts[4] = {9.95, 4.55, 13.25, 22.35};
-    float mealCost = mealCosts[rand() % 4];
+    float mealCost = mealCosts[randIndex];
 
-    // Calculate final (tip is on the post-tax amount)
-    float finalCost = mealCost + (mealCost * tax);
-    finalCost += finalCost * tip;
+    // Calculate final (tip is on the pre-tax amount)
+    float taxCost = mealCost * tax;
+    float tipCost = mealCost * tip;
+    float finalCost = mealCost + taxCost + tipCost;
 
     // Print formatted results
-    printf("Meal cost: $%.2f\n", mealCost);
-    printf("Tax amount: %.0f%%\n", tax * 100);
-    printf("Tip amount: %.0f%%\n", tip * 100);
+    printf("Cost of %s: $%.2f\n", meals[randIndex], mealCost);
+    printf("Tax amount: $%.2f\n", taxCost);
+    printf("Tip amount: $%.2f\n", tipCost);
     printf("Total: $%.2f\n", finalCost);
 }
